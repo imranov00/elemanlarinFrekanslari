aced17bf72aff750262f315c689f0a150fbe9faa
İmranov <semtexpert@gmail.com>
2024-01-04 16:43:59 +0300

    version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
 public class Main {
+
+    static int[] sortArray(int[] numbers) {
+        int[] tempArr = Arrays.copyOf(numbers, numbers.length);
+
+        for (int i = 0; i < tempArr.length - 1; i++) {
+            for (int j = i + 1; j < tempArr.length; j++) {
+                if (tempArr[j] < tempArr[i]) {
+                    int tempNumber = tempArr[i];
+                    tempArr[i] = tempArr[j];
+                    tempArr[j] = tempNumber;
+                }
+            }
+        }
+        return tempArr;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
+
+        arr = sortArray(arr);
+
+        int counter = 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] == arr[i]) {
+                counter++;
+            } else {
+                System.out.println(arr[i] + " sayısı " + counter + " kadar tekrar etmektedir.");
+                counter = 1;
+            }
+        }
+        System.out.println(arr[arr.length - 1] + " sayısı " + counter + " kadar tekrar etmektedir.");
     }
 }
